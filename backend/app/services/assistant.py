18a7378dc6494d7aaf5c0882908bad61918c25a6
Alonso <gomez.alonso07@gmail.com>
2026-04-21 11:51:04 +0200

    Funciones en assistant.py
    Lógica para gestionar el CRUD de los asistentes.

--- backend/app/services/assistant.py
+++ backend/app/services/assistant.py
@@ -1,1 +1,25 @@
-# Assistant
+from sqlalchemy.orm import Session
+from .. import models, schemas
+
+def create_assistant(db: Session, assistant: schemas.AssistantCreate):
+    db_assistant = models.Assistant(**assistant.dict())
+    db.add(db_assistant)
+    commit_db(db)
+    return db_assistant
+
+def get_assistants(db: Session):
+    return db.query(models.Assistant).all()
+
+def delete_assistant(db: Session, assistant_id: int):
+    assistant = db.query(models.Assistant).filter(models.Assistant.id == assistant_id).first()
+    if assistant:
+        db.delete(assistant)
+        commit_db(db)
+    return assistant
+
+def commit_db(db: Session):
+    try:
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
